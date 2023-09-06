6b5415ea5bea231f781ca9214a49d65314de73af
Andrii Ozhytchuk <andrii_ozhytchuk@epam.com>
2023-09-06 13:45:50 +0300

    test

---
+++ simplewpplugin.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name:  Simple Test
+Plugin URI:   https://www.test.com
+Description:  A short little description of the plugin. It will be displayed on the Plugins page in WordPress admin area.
+Version:      1.0.0
+Author:       Andrii Ozhytchuk
+Author URI:   https://www.test.com
+*/
+
+if ( ! is_admin() ) {
+	add_action(
+		'init',
+		function () {
+			echo 'hello';
+			die();
+		}
+	);
+}
+
+
